e449248c7504cadf288f4d9b2491540f8fb75834
camera-2018 <2907618001@qq.com>
2020-07-26 18:09:41 +0800

    add1

---
+++ 1.java
@@ -0,0 +1,5 @@
+public class 1{
+    public static void main(String[] args) {
+      System.out.print(1);
+    }
+}
---
+++ Test.java
@@ -0,0 +1,46 @@
+public class Test{
+	public static void main(String[] args) {
+		// byte b = 1;
+		// System.out.println(b);
+
+		// short s = 2;
+		// System.out.println(s);
+
+		// int i = 4;		
+		// System.out.println(i);
+
+		// long l = 79l;
+		// System.out.println(l);
+
+		// float f = 1.23f;
+		// System.out.println(f);
+
+		// double d = 1.56;
+		// System.out.println(d);
+
+		// char c = 'a';
+		// System.out.println(c);
+
+		// boolean b1 = false;
+		// System.out.println(b1);
+
+ 	// 	String str = "hello"+" world";
+ 	// 	System.out.println(str);
+		// short s = 2;
+		// s = (short)(s + 3);
+		// System.out.print(s);
+		// int m = 16496;
+		// int n = 5000;
+		// int k = 100;
+		// System.out.print( m > n ? ( m > k ? m : k) : ( n > k ? n : k ));
+		// for (int j = 0;j<3 ;j++ ) {
+		// 	for (int i=0 ;i<9 ;i++ ) {
+		// 		if (i>2) {
+		// 			break;
+		// 		}
+		// 		System.out.print(i);
+		// 	}
+		// }
+		System.out.print(1<<63);
+	}
+}
